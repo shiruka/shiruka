894c753cc9c5b9babf00c7cd444714a54d90f3e2
Hasan Demirtaş <utsukushihito@outlook.com>
2020-11-14 16:57:17 +0300

    Update ShirukaConsoleParser.java

--- src/main/java/io/github/shiruka/shiruka/ShirukaConsoleParser.java
+++ src/main/java/io/github/shiruka/shiruka/ShirukaConsoleParser.java
@@ -68,104 +68,11 @@ final class ShirukaConsoleParser {
   @Nullable
   static OptionSet parseOptions(@NotNull final String[] args) {
     ShirukaConsoleParser.PARSER.acceptsAll(Arrays.asList("?", "help"), "Show the help");
-    ShirukaConsoleParser.PARSER.acceptsAll(Arrays.asList("c", "config"), "Properties file to use")
-      .withRequiredArg()
-      .ofType(File.class)
-      .defaultsTo(new File("server.properties"))
-      .describedAs("Properties file");
     ShirukaConsoleParser.PARSER.acceptsAll(Arrays.asList("P", "plugins"), "Plugin directory to use")
       .withRequiredArg()
       .ofType(File.class)
       .defaultsTo(new File("plugins"))
       .describedAs("Plugin directory");
-    ShirukaConsoleParser.PARSER.acceptsAll(Arrays.asList("h", "host", "server-ip"), "Host to listen on")
-      .withRequiredArg()
-      .ofType(String.class)
-      .describedAs("Hostname or IP");
-    ShirukaConsoleParser.PARSER.acceptsAll(Arrays.asList("W", "world-dir", "universe", "world-container"), "World container")
-      .withRequiredArg()
-      .ofType(File.class)
-      .defaultsTo(new File("."))
-      .describedAs("Directory containing worlds");
-    ShirukaConsoleParser.PARSER.acceptsAll(Arrays.asList("w", "world", "level-name"), "World name")
-      .withRequiredArg()
-      .ofType(String.class)
-      .describedAs("World name");
-    ShirukaConsoleParser.PARSER.acceptsAll(Arrays.asList("p", "port", "server-port"), "Port to listen on")
-      .withRequiredArg()
-      .ofType(Integer.class)
-      .describedAs("Port");
-    ShirukaConsoleParser.PARSER.acceptsAll(Arrays.asList("o", "online-mode"), "Whether to use online authentication")
-      .withRequiredArg()
-      .ofType(Boolean.class)
-      .describedAs("Authentication");
-    ShirukaConsoleParser.PARSER.acceptsAll(Arrays.asList("s", "size", "max-players"), "Maximum amount of players")
-      .withRequiredArg()
-      .ofType(Integer.class)
-      .describedAs("Server size");
-    ShirukaConsoleParser.PARSER.acceptsAll(Arrays.asList("d", "date-format"), "Format of the date to display in the console (for log entries)")
-      .withRequiredArg()
-      .ofType(SimpleDateFormat.class)
-      .describedAs("Log date format");
-    ShirukaConsoleParser.PARSER.acceptsAll(Collections.singletonList("log-pattern"), "Specfies the log filename pattern")
-      .withRequiredArg()
-      .ofType(String.class)
-      .defaultsTo("server.log")
-      .describedAs("Log filename");
-    ShirukaConsoleParser.PARSER.acceptsAll(Collections.singletonList("log-limit"), "Limits the maximum size of the log file (0 = unlimited)")
-      .withRequiredArg()
-      .ofType(Integer.class)
-      .defaultsTo(0)
-      .describedAs("Max log size");
-    ShirukaConsoleParser.PARSER.acceptsAll(Collections.singletonList("log-count"), "Specified how many log files to cycle through")
-      .withRequiredArg()
-      .ofType(Integer.class)
-      .defaultsTo(1)
-      .describedAs("Log count");
-    ShirukaConsoleParser.PARSER.acceptsAll(Collections.singletonList("log-append"), "Whether to append to the log file")
-      .withRequiredArg()
-      .ofType(Boolean.class)
-      .defaultsTo(true)
-      .describedAs("Log append");
-    ShirukaConsoleParser.PARSER.acceptsAll(Collections.singletonList("log-strip-color"), "Strips color codes from log file");
-    ShirukaConsoleParser.PARSER.acceptsAll(Arrays.asList("b", "bukkit-settings"), "File for bukkit settings")
-      .withRequiredArg()
-      .ofType(File.class)
-      .defaultsTo(new File("bukkit.yml"))
-      .describedAs("Yml file");
-    ShirukaConsoleParser.PARSER.acceptsAll(Arrays.asList("C", "commands-settings"), "File for command settings")
-      .withRequiredArg()
-      .ofType(File.class)
-      .defaultsTo(new File("commands.yml"))
-      .describedAs("Yml file");
-    ShirukaConsoleParser.PARSER.acceptsAll(Collections.singletonList("forceUpgrade"), "Whether to force a world upgrade");
-    ShirukaConsoleParser.PARSER.acceptsAll(Collections.singletonList("eraseCache"), "Whether to force cache erase during world upgrade");
-    ShirukaConsoleParser.PARSER.acceptsAll(Collections.singletonList("nogui"), "Disables the graphical console");
-    ShirukaConsoleParser.PARSER.acceptsAll(Collections.singletonList("nojline"), "Disables jline and emulates the vanilla console");
-    ShirukaConsoleParser.PARSER.acceptsAll(Collections.singletonList("noconsole"), "Disables the console");
-    ShirukaConsoleParser.PARSER.acceptsAll(Arrays.asList("v", "version"), "Show the CraftBukkit Version");
-    ShirukaConsoleParser.PARSER.acceptsAll(Collections.singletonList("demo"), "Demo mode");
-    // Spigot Start
-    ShirukaConsoleParser.PARSER.acceptsAll(Arrays.asList("S", "spigot-settings"), "File for spigot settings")
-      .withRequiredArg()
-      .ofType(File.class)
-      .defaultsTo(new File("spigot.yml"))
-      .describedAs("Yml file");
-    // Spigot End
-    // Paper Start
-    ShirukaConsoleParser.PARSER.acceptsAll(Arrays.asList("paper", "paper-settings"), "File for paper settings")
-      .withRequiredArg()
-      .ofType(File.class)
-      .defaultsTo(new File("paper.yml"))
-      .describedAs("Yml file");
-    // Paper end
-    // Paper start
-    ShirukaConsoleParser.PARSER.acceptsAll(Collections.singletonList("server-name"), "Name of the server")
-      .withRequiredArg()
-      .ofType(String.class)
-      .defaultsTo("Unknown Server")
-      .describedAs("Name");
-    // Paper end
     try {
       return ShirukaConsoleParser.PARSER.parse(args);
     } catch (final OptionException e) {

053c1274a72679ef73e7274aced9efc1db6644e4
Hasan Demirtaş <utsukushihito@outlook.com>
2020-11-26 18:57:40 +0300

    Update ListTagBasic.java

--- src/main/java/io/github/shiruka/shiruka/nbt/list/ListTagBasic.java
+++ src/main/java/io/github/shiruka/shiruka/nbt/list/ListTagBasic.java
@@ -109,15 +109,15 @@ public final class ListTagBasic implements ListTag {
     return this.original.stream();
   }
 
+  @NotNull
+  @Override
+  public List<Tag> all() {
+    return Collections.unmodifiableList(this.original);
+  }
+
   @Override
   public byte listType() {
     return this.listType;
-  }
-
-  @NotNull
-  @Override
-  public List<Tag> getList() {
-    return Collections.unmodifiableList(this.original);
   }
 
   @Override

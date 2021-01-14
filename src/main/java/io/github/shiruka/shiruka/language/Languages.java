d2871565cd46492e46a36dfde79adde337920350
Hasan Demirtaş <utsukushihito@outlook.com>
2021-01-15 02:02:21 +0300

    Update Languages.java

--- src/main/java/io/github/shiruka/shiruka/language/Languages.java
+++ src/main/java/io/github/shiruka/shiruka/language/Languages.java
@@ -36,7 +36,6 @@ import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;

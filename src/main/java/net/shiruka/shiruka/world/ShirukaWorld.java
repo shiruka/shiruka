4b6f66216cff392eb51f82cbf3e9ac0095b090da
Hasan Demirtaş <utsukushihito@outlook.com>
2021-04-15 18:57:34 +0300

    Update ShirukaWorld.java

--- src/main/java/net/shiruka/shiruka/world/ShirukaWorld.java
+++ src/main/java/net/shiruka/shiruka/world/ShirukaWorld.java
@@ -27,15 +27,19 @@ package net.shiruka.shiruka.world;
 
 import java.time.Duration;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.extern.log4j.Log4j2;
 import net.shiruka.api.base.BlockPosition;
+import net.shiruka.api.block.Block;
 import net.shiruka.api.metadata.MetadataValue;
 import net.shiruka.api.plugin.Plugin;
 import net.shiruka.api.registry.Resourced;
+import net.shiruka.api.world.Chunk;
+import net.shiruka.api.world.GameRule;
 import net.shiruka.api.world.World;
 import net.shiruka.shiruka.ShirukaServer;
 import org.jetbrains.annotations.NotNull;
@@ -89,6 +93,51 @@ public final class ShirukaWorld implements World {
   @Getter
   @Setter
   private Duration time;
+
+  @NotNull
+  @Override
+  public Chunk generateEmptyChunk(final int x, final int z) {
+    throw new UnsupportedOperationException(" @todo #1:10m Implement ShirukaWorld#generateEmptyChunk.");
+  }
+
+  @NotNull
+  @Override
+  public Block getBlock(@NotNull final BlockPosition position) {
+    throw new UnsupportedOperationException(" @todo #1:10m Implement ShirukaWorld#getBlock.");
+  }
+
+  @NotNull
+  @Override
+  public Optional<Chunk> getChunk(final int x, final int z) {
+    throw new UnsupportedOperationException(" @todo #1:10m Implement ShirukaWorld#getChunk.");
+  }
+
+  @NotNull
+  @Override
+  public <T> T getGameRule(@NotNull final GameRule<T> gameRule) {
+    throw new UnsupportedOperationException(" @todo #1:10m Implement ShirukaWorld#getGameRule.");
+  }
+
+  @NotNull
+  @Override
+  public Chunk getOrGenerateChunk(final int x, final int z) {
+    throw new UnsupportedOperationException(" @todo #1:10m Implement ShirukaWorld#getOrGenerateChunk.");
+  }
+
+  @Override
+  public void save() {
+    throw new UnsupportedOperationException(" @todo #1:10m Implement ShirukaWorld#save.");
+  }
+
+  @Override
+  public <T> void setGameRule(@NotNull final GameRule<T> gameRule, @NotNull final T value) {
+    throw new UnsupportedOperationException(" @todo #1:10m Implement ShirukaWorld#setGameRule.");
+  }
+
+  @Override
+  public void unloadChunk(final int x, final int z) {
+    throw new UnsupportedOperationException(" @todo #1:10m Implement ShirukaWorld#unloadChunk.");
+  }
 
   @NotNull
   @Override

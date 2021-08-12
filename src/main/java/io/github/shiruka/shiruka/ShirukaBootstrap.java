dd5a01007313b251aab01f7b831bb94dc7fcbbdf
Hasan Demirtaş <utsukushihito@outlook.com>
2021-08-12 17:26:25 +0300

    log formatter.

--- src/main/java/io/github/shiruka/shiruka/ShirukaBootstrap.java
+++ src/main/java/io/github/shiruka/shiruka/ShirukaBootstrap.java
@@ -8,6 +8,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.security.NoSuchAlgorithmException;
 import java.text.MessageFormat;
+import java.util.logging.LogManager;
 import java.util.logging.Logger;
 import lombok.SneakyThrows;
 
@@ -15,6 +16,11 @@ import lombok.SneakyThrows;
  * a class that represents bootstrap for running the Shiru ka.
  */
 public final class ShirukaBootstrap {
+
+  /**
+   * the java logger.
+   */
+  private static final Logger LOGGER = Logger.getLogger("Shiru ka");
 
   /**
    * ctor.
@@ -26,8 +32,12 @@ public final class ShirukaBootstrap {
    * runs first when the application starts.
    *
    * @param args the args to pass into application.
+   *
+   * @throws IOException if something goes wrong when reading property files.
    */
-  public static void main(final String[] args) {
+  public static void main(final String[] args) throws IOException {
+    LogManager.getLogManager().readConfiguration(
+      ShirukaBootstrap.class.getResourceAsStream("/java.logger.properties"));
     ShirukaBootstrap.loadDependencies();
   }
 
@@ -41,27 +51,25 @@ public final class ShirukaBootstrap {
     if (Files.notExists(libs)) {
       Files.createDirectories(libs);
     }
-    final var logger = Logger.getLogger("Shiru ka");
-    logger.info("Loading dependencies, this might take a while...");
+    ShirukaBootstrap.LOGGER.info("Loading dependencies, this might take a while...");
     try {
       ApplicationBuilder.appending("Shiru ka")
         .logger(new ProcessLogger() {
           @Override
           public void log(final String message, final Object... args) {
-            logger.info(MessageFormat.format(message, args));
+            ShirukaBootstrap.LOGGER.info(MessageFormat.format(message, args));
           }
 
           @Override
           public void debug(final String message, final Object... args) {
-            logger.fine(MessageFormat.format(message, args));
           }
         })
         .downloadDirectoryPath(libs)
         .build();
     } catch (final IOException | ReflectiveOperationException | URISyntaxException | NoSuchAlgorithmException e) {
       e.printStackTrace();
-      logger.warning("Shiru ka failed to load its dependencies correctly!");
-      logger.warning("This error should be reported at https://github.com/shiruka/shiruka/issues");
+      ShirukaBootstrap.LOGGER.warning("Shiru ka failed to load its dependencies correctly!");
+      ShirukaBootstrap.LOGGER.warning("This error should be reported at https://github.com/shiruka/shiruka/issues");
     }
   }
 }

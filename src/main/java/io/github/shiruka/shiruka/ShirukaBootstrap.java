3f07a908df2f099b1792685e33fc8c6168c8f4c7
Hasan Demirtaş <utsukushihito@outlook.com>
2021-08-12 16:23:38 +0300

    bootstrap.

---
+++ src/main/java/io/github/shiruka/shiruka/ShirukaBootstrap.java
@@ -0,0 +1,21 @@
+package io.github.shiruka.shiruka;
+
+/**
+ * a class that represents bootstrap for running the Shiru ka.
+ */
+public final class ShirukaBootstrap {
+
+  /**
+   * ctor.
+   */
+  private ShirukaBootstrap() {
+  }
+
+  /**
+   * runs first when the application starts.
+   *
+   * @param args the args to pass into application.
+   */
+  public static void main(final String[] args) {
+  }
+}

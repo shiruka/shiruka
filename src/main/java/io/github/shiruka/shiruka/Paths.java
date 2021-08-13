0be093a1469f6085a77c7460fc902b195cf64aa0
Hasan Demirtaş <utsukushihito@outlook.com>
2021-08-13 17:37:39 +0300

    delombok

--- src/main/java/io/github/shiruka/shiruka/Paths.java
+++ src/main/java/io/github/shiruka/shiruka/Paths.java
@@ -2,24 +2,20 @@ package io.github.shiruka.shiruka;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
-import lombok.SneakyThrows;
 import org.jetbrains.annotations.NotNull;
 
 /**
  * an utility class that contains constant paths.
  */
 final class Paths {
-
   /**
    * the here.
    */
   private static final String HERE = System.getProperty("user.dir");
-
   /**
    * the here path.
    */
   private static final Path HERE_PATH = Path.of(Paths.HERE);
-
   /**
    * the libs path.
    */
@@ -37,11 +33,14 @@ final class Paths {
    * @return libs path.
    */
   @NotNull
-  @SneakyThrows
   static Path getLibsPath() {
-    if (Files.notExists(Paths.LIBS_PATH)) {
-      Files.createDirectories(Paths.LIBS_PATH);
+    try {
+      if (Files.notExists(Paths.LIBS_PATH)) {
+        Files.createDirectories(Paths.LIBS_PATH);
+      }
+      return Paths.LIBS_PATH;
+    } catch (final java.lang.Throwable $ex) {
+      throw lombok.Lombok.sneakyThrow($ex);
     }
-    return Paths.LIBS_PATH;
   }
 }
--- src/main/java/io/github/shiruka/shiruka/ShirukaServer.java
+++ src/main/java/io/github/shiruka/shiruka/ShirukaServer.java
@@ -2,26 +2,33 @@ package io.github.shiruka.shiruka;
 
 import io.github.shiruka.api.Provider;
 import io.github.shiruka.api.Server;
-import lombok.Getter;
-import lombok.experimental.Accessors;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 /**
  * a class that represents Shiru ka server.
  */
-@Accessors(fluent = true)
 public final class ShirukaServer implements Server {
+  /**
+   * the logger.
+   */
+  private final Logger logger = LogManager.getLogger("Shiru ka");
+  /**
+   * the provider.
+   */
+  private final Provider provider = Provider.create();
 
   /**
    * the logger.
    */
-  @Getter
-  private final Logger logger = LogManager.getLogger("Shiru ka");
+  public Logger logger() {
+    return this.logger;
+  }
 
   /**
    * the provider.
    */
-  @Getter
-  private final Provider provider = Provider.create();
+  public Provider provider() {
+    return this.provider;
+  }
 }
